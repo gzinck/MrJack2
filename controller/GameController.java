909da418cbfb19fe96587af941abbc5e726dadd6
gzinck <graeme.zinck@gmail.com>
2018-10-14 13:45:58 -0300

    Started adding the controller test cases.

--- controller/GameController.java
+++ controller/GameController.java
@@ -1,5 +1,16 @@
 package controller;
 
 public class GameController {
-
+	public void initializeGame()  {
+		
+	}
+	public void continueGame() {
+		
+	}
+	public void updateTurn() {
+		
+	}
+	public boolean gameIsOver() {
+		return false;
+	}
 }
