003749c553031cc16d4de8b2ff797354642911ed
charlesjobin <44141195+charlesjobin@users.noreply.github.com>
2018-10-14 14:17:12 -0300

    model classes
    added a bunch of model classes

--- model/Table.java
+++ model/Table.java
@@ -1,5 +1,20 @@
 package model;
 
-public class Table {
 
+
+public class Table 
+{
+	public GameBoard board;
+	public String[] characters = new String[4];
+	public AlibiDeck alibis;
+	public WitnessCard witness;
+	
+	public static void discardCharacter(String character)
+	{
+		
+	}
+	public static void initializeTable()
+	{
+		
+	}
 }
