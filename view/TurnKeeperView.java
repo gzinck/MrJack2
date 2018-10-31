1d5c6723103e0bcd0484746f44162d96aa681b31
gzinck <graeme.zinck@gmail.com>
2018-10-31 16:26:13 -0300

    Added comments to the last classes in the view.

--- view/TurnKeeperView.java
+++ view/TurnKeeperView.java
@@ -1,7 +1,33 @@
 package view;
 
+/**
+ * This interface allows the controllers to change the turn status
+ * view without having access to any of the other methods (or
+ * having dependencies that shouldn't exist).
+ * 
+ * @author Graeme Zinck and Charles Jobin
+ * @version 1.0
+ *
+ */
 public interface TurnKeeperView {
+	/**
+	 * Updates the player name registered on the turn keeper.
+	 * 
+	 * @param playerName the name of the player to go next
+	 */
 	public void updatePlayer(String playerName);
+	
+	/**
+	 * Updates the turn number registered on the turn keeper.
+	 * 
+	 * @param turn the turn number
+	 */
 	public void updateTurn(int turn);
+	
+	/**
+	 * Updates the round number registered on the turn keeper.
+	 * 
+	 * @param round the round number
+	 */
 	public void updateRound(int round);
 }
